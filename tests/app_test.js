fb653c595e98d02d011f6ead8767afcd09130dc1
ssankar123 <sankarlingam1994@gmail.com>
2018-06-02 15:15:42 +0530

    Update app_test.js

--- tests/app_test.js
+++ tests/app_test.js
@@ -30,4 +30,46 @@ describe('Basic routes tests', function() {
     })
 })
 
+/*
+ * NodeJS code.
+ */
 
+// Required modules.
+var express = require('express'),
+    http = require('http'),
+    formidable = require('formidable'),
+    fs = require('fs'),
+    path = require('path');
+
+var app = express();
+
+// All your express server code goes here.
+// ...
+
+// Upload route.
+app.post('/upload', function(req, res) {
+    var form = new formidable.IncomingForm();
+    form.parse(req, function(err, fields, files) {
+        // `file` is the name of the <input> field of type `file`
+        var old_path = files.file.path,
+            file_size = files.file.size,
+            file_ext = files.file.name.split('.').pop(),
+            index = old_path.lastIndexOf('/') + 1,
+            file_name = old_path.substr(index),
+            new_path = path.join(process.env.PWD, '/uploads/', file_name + '.' + file_ext);
+
+        fs.readFile(old_path, function(err, data) {
+            fs.writeFile(new_path, data, function(err) {
+                fs.unlink(old_path, function(err) {
+                    if (err) {
+                        res.status(500);
+                        res.json({'success': false});
+                    } else {
+                        res.status(200);
+                        res.json({'success': true});
+                    }
+                });
+            });
+        });
+    });
+});
