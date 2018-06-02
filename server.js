2338bc1239a9f807b9a0db7b029f997bd3512e0a
ssankar123 <sankarlingam1994@gmail.com>
2018-06-02 11:10:51 +0530

    Create server.js

---
+++ server.js
@@ -0,0 +1,55 @@
+
+
+var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080
+var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
+ 
+server.listen(server_port, server_ip_address, function () {
+  console.log( "Listening on " + server_ip_address + ", port " + server_port )
+});
+
+var fs = require("fs"),
+    http = require("http"),
+    url = require("url"),
+    path = require("path");
+
+http.createServer(function (req, res) {
+  if (req.url != "/trailer.mp4") {
+    res.writeHead(200, { "Content-Type": "text/html" });
+    res.end('<video src="http://localhost:8888/trailer.mp4" controls></video>');
+  } else {
+    var file = path.resolve(__dirname,"trailer.mp4");
+    fs.stat(file, function(err, stats) {
+      if (err) {
+        if (err.code === 'ENOENT') {
+          // 404 Error if file not found
+          return res.sendStatus(404);
+        }
+      res.end(err);
+      }
+      var range = req.headers.range;
+      if (!range) {
+       // 416 Wrong range
+       return res.sendStatus(416);
+      }
+      var positions = range.replace(/bytes=/, "").split("-");
+      var start = parseInt(positions[0], 10);
+      var total = stats.size;
+      var end = positions[1] ? parseInt(positions[1], 10) : total - 1;
+      var chunksize = (end - start) + 1;
+
+      res.writeHead(206, {
+        "Content-Range": "bytes " + start + "-" + end + "/" + total,
+        "Accept-Ranges": "bytes",
+        "Content-Length": chunksize,
+        "Content-Type": "video/mp4"
+      });
+
+      var stream = fs.createReadStream(file, { start: start, end: end })
+        .on("open", function() {
+          stream.pipe(res);
+        }).on("error", function(err) {
+          res.end(err);
+        });
+    });
+  }
+}).server.listen;
